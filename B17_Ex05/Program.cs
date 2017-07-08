f2ac5018827a06f33d893f36ddbf4e2609449a1b
LiadBerko <liad.berko@gmail.com>
2017-07-08 13:18:59 +0300

    implemented colors form

---
+++ B17_Ex05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B17_Ex05
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
